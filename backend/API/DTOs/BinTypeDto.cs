6abba9b10a08983b51b0bf786e5ed9c8d19975f7
Yang <jossieyang@gmail.com>
2021-01-29 12:37:16 -0500

    fix backend confliction

---
+++ backend/API/DTOs/BinTypeDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class BinTypeDto
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    }
+}
---
+++ backend/API/DTOs/CreateBinTypeDto.cs
@@ -0,0 +1,7 @@
+namespace API.DTOs
+{
+    public class CreateBinTypeDto
+    {
+        public string TypeName { get; set; }
+    }
+}
