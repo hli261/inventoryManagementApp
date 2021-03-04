9d87b0e5d0d43b6ec725415b90aa4225f212dcf6
xweng1016 <54854234+xweng1016@users.noreply.github.com>
2021-03-04 17:20:36 -0500

    receiving

--- backend/API/Entities/Receiving.cs
+++ backend/API/Entities/Receiving.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace API.Entities
 {
     public class Receiving
     {
         public int Id { get; set; }
+        public Shipping Shipping { get; set; }
+        public DateTime ReceiveDate { get; set; } = DateTime.UtcNow;
+        public AppUser Receiver { get; set; }
+
+        public string Status { get; set; }
+
+        public string ROnumber { get; set; }
+
+        public ReceivingItem ReceivingItems { get; set; }
     }
 }
--- backend/API/Entities/ReceivingItem.cs
+++ backend/API/Entities/ReceivingItem.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace API.Entities
 {
     public class ReceivingItem
     {
         public int Id { get; set; }
+        public int OrderQty { get; set; }
+        public int ReceiveQty { get; set; }
+        public int DiffQty { get; set; }
+
+        public DateTime ExpireDate {get;set;}
+
+
     }
 }
