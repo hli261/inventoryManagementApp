771c14bf25b0bd345710807c6f8ef7a13793b594
hli261 <50968650+hli261@users.noreply.github.com>
2021-03-07 20:16:40 -0500

    receivingcreate

--- frontend/src/app/_services/receiving.service.ts
+++ frontend/src/app/_services/receiving.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ReceivingCreate } from '../_models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,11 @@ export class ReceivingService {
   constructor(private http: HttpClient) { 
   }
 
-
-
+  getCreate(PONumber:any, venderNo:any, shippingNumber:any){
+    return this.http.get<ReceivingCreate>(`${environment.apiUrl}/api/Receiving/receivingOrder?PONumber=${PONumber}&venderNo=${venderNo}&shippingNumber=${shippingNumber}`);
+    
+  }
+ 
   getShippingMethod(): Observable<any>{
     return this.http.get<any>(`${environment.apiUrl}/api/Vender/shippingMethods`);
   }
