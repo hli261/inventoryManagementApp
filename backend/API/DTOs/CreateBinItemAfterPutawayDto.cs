038274a671e8f7064595c6837c23fec4d17c3157
XP CANADA <xingpancanada@users.noreply.github.com>
2021-03-22 18:32:07 -0400

    add createBinItemAfterPutaway

---
+++ backend/API/DTOs/CreateBinItemAfterPutawayDto.cs
@@ -0,0 +1,18 @@
+namespace API.DTOs
+{
+    public class CreateBinItemAfterPutawayDto
+    {
+         // public Bin Bin { get; set; }
+        // public Item Item { get; set; }
+
+        public int Quantity { get; set; }
+  
+        public string FromBinCode { get; set; }
+
+        public string DestinationBinCode { get; set; }
+
+        public string ItemNumber { get; set; }    
+
+        public string LotNumber { get; set; } 
+    }
+}
